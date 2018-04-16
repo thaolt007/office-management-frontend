6b01e3bd2a4502aa76f3fe00eb5d5269c32f4a27
minhlucvan <minh.lucvan@nccsoft.vn>
2018-04-16 13:57:01 +0700

    adding initial client project

---
+++ client/src/app/dashboard-widget/pricing-plan/pricing-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ncc-pricing-plan',
+  templateUrl: './pricing-plan.component.html',
+  styleUrls: ['./pricing-plan.component.scss']
+})
+export class PricingPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
