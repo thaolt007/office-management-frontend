7022e863d6c833ab6d41a958384cdace063d6cf6
Trung Xabi <thanhtrungnait88@gmail.com>
2018-04-19 18:05:42 +0700

    update login office

--- client/src/app/dashboard-crm/dashboard-crm.module.ts
+++ client/src/app/dashboard-crm/dashboard-crm.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router'; 
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardCrmComponent } from './dashboard-crm.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
